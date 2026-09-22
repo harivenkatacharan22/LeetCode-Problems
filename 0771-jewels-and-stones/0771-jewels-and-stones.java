fb7f1501ae25eecb19e245ffbaa6153760191ca9
HARI VENKATA CHARAN <harivenkatacharan@gmail.com>
2026-09-22 09:53:13 +0530

    Time: 1 ms (56.43%), Space: 42.9 MB (91.72%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int c=0;
+        for(char ch1:jewels.toCharArray()){
+            for(char ch2:stones.toCharArray()){
+                if(ch1==ch2){
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
+}
